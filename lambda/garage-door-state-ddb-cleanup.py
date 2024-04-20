b47e38354ed2760c68d367ad3b9dcaf7e1098371
gamename <tennis.n.smith@gmail.com>
2024-04-20 18:32:28 -0500

    convert to lambda

--- lambda/garage-door-state-ddb-cleanup.py
+++ lambda/garage-door-state-ddb-cleanup.py
@@ -5,13 +5,20 @@ import boto3
 import pytz
 from botocore.exceptions import ClientError
 
+# Initialize DynamoDB outside of the handler to take advantage of Lambda's execution context reuse
+dynamodb = boto3.resource('dynamodb')
 
-def delete_old_entries(table_name, days_to_keep, dynamodb=None):
-    # Validate days_to_keep
+
+def delete_old_entries(table_name, days_to_keep, dynamodb):
+    # Convert days_to_keep to an integer and validate
     try:
         days_to_keep = int(days_to_keep)
     except ValueError:
-        raise ValueError("The 'days_to_keep' parameter must be an integer.")
+        print("The 'days_to_keep' parameter must be an integer.")
+        return {
+            'statusCode': 400,
+            'body': "The 'days_to_keep' parameter must be an integer."
+        }
 
     # Get the current time in US Central Time Zone
     central_tz = pytz.timezone('US/Central')
@@ -28,26 +35,30 @@ def delete_old_entries(table_name, days_to_keep, dynamodb=None):
             }
         )
         items = response.get('Items', [])
-
         for item in items:
             # Delete items found in the scan
-            print(f"Deleting item with timestamp: {item['garage_door_status_timestamp']}")
             table.delete_item(
                 Key={
                     'id': item['id'],
                     'garage_door_status_timestamp': item['garage_door_status_timestamp']
                 }
             )
-        print(f"Total {len(items)} items deleted.")
+        return {
+            'statusCode': 200,
+            'body': f"Total {len(items)} items deleted."
+        }
     except ClientError as e:
         print(f"Failed to delete old entries: {e.response['Error']['Message']}")
+        return {
+            'statusCode': 500,
+            'body': f"Failed to delete old entries: {e.response['Error']['Message']}"
+        }
 
 
-if __name__ == '__main__':
-    ddb = boto3.resource('dynamodb')
+def lambda_handler(event, context):
     # Fetch configuration from environment variables
     table_name = os.getenv('DYNAMODB_TABLE', 'default-table-name')
     days_to_keep = os.getenv('DAYS_TO_KEEP', '3')
-
-    # Call the function with environment variables
-    delete_old_entries(table_name, days_to_keep, dynamodb=ddb)
+    # Call the delete_old_entries function with the DynamoDB resource
+    result = delete_old_entries(table_name, days_to_keep, dynamodb)
+    return result
